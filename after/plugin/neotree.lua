8551be5ec724ff0292480d09fa31a13da02fda55
sixtenl <landysixten@gmail.com>
2023-06-10 02:41:21 +0200

    Update neotree.lua

--- after/plugin/neotree.lua
+++ after/plugin/neotree.lua
@@ -1,5 +1,19 @@
 require("neo-tree").setup({
     default_component_configs = {
+        diagnostics = {
+            symbols = {
+                hint = "󰌵",
+                info = " ",
+                warn = " ",
+                error = " ",
+            },
+            highlights = {
+                hint = "DiagnosticSignHint",
+                info = "DiagnosticSignInfo",
+                warn = "DiagnosticSignWarn",
+                error = "DiagnosticSignError",
+            },
+        },
         icon = {
             folder_empty = "󰜌",
             folder_empty_open = "󰜌",
