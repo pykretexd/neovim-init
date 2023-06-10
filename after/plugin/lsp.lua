62437734253ea0c7edd4a378e368b8742ca033e6
sixtenl <landysixten@gmail.com>
2023-06-10 02:39:53 +0200

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -28,12 +28,11 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.set_preferences({
-    suggest_lsp_servers = false,
     sign_icons = {
-        error = 'E',
-        warn = 'W',
-        hint = 'H',
-        info = 'I'
+        hint = "󰌵",
+        info = " ",
+        warn = " ",
+        error = " ",
     }
 })
 
