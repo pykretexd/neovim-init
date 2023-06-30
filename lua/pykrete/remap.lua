a011d2829a969acddb332cd4435cc18229d41bd5
sixtenl <landysixten@gmail.com>
2023-06-30 13:35:12 +0200

    Add Neotree toggle keybind

--- lua/pykrete/remap.lua
+++ lua/pykrete/remap.lua
@@ -10,3 +10,5 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
+
+vim.keymap.set('n', '<leader>pv', '<Cmd>Neotree toggle<CR>')
