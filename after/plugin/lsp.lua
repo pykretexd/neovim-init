1206e3c7a2fccf3105339193eb111808d7fb3047
sixtenl <landysixten@gmail.com>
2023-07-11 18:41:52 +0200

    Update lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -61,6 +61,7 @@ lsp.format_on_save({
     ['lua_ls'] = {'lua'},
     ['rust_analyzer'] = {'rust'},
     ['pylsp'] = {'python'},
+    ['tsserver'] = {'typescript'}
     -- if you have a working setup with null-ls
     -- you can specify filetypes it can format.
     -- ['null-ls'] = {'javascript', 'typescript'},
