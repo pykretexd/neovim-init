b176bd7fede2dd2c03f61f65f0db5694964fd7b0
pykrete <landysixten@gmail.com>
2025-02-26 12:09:13 +0100

    Added format on save

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -11,6 +11,22 @@ lspconfig_defaults.capabilities = vim.tbl_deep_extend(
     require('cmp_nvim_lsp').default_capabilities()
 )
 
+local buffer_autoformat = function(bufnr)
+    local group = 'lsp_autoformat'
+    vim.api.nvim_create_augroup(group, {clear = false})
+    vim.api.nvim_clear_autocmds({group = group, buffer = bufnr})
+
+    vim.api.nvim_create_autocmd('BufWritePre', {
+        buffer = bufnr,
+        group = group,
+        desc = 'LSP format on save',
+        callback = function()
+            -- note: do not enable async formatting
+            vim.lsp.buf.format({async = false, timeout_ms = 10000})
+        end,
+    })
+end
+
 -- This is where you enable features that only work
 -- if there is a language server active in the file
 vim.api.nvim_create_autocmd('LspAttach', {
@@ -27,5 +43,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
         vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
         vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
         vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+
+        local id = vim.tbl_get(event, 'data', 'client_id')
+        local client = id and vim.lsp.get_client_by_id(id)
+        if client == nil then
+            return
+        end
+
+        -- make sure there is at least one client with formatting capabilities
+        if client.supports_method('textDocument/formatting') then
+            buffer_autoformat(event.buf)
+        end
     end,
 })
